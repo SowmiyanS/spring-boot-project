2c22c2c4fbb75a355634ac44df577c6f4592f73c
SowmiyanS <sowmiyan9491@gmail.com>
2024-02-18 20:43:07 +0530

    Basic spring boot application, uses Beans.

---
+++ src/main/java/app/web/appsowmin/spring/HelloController.java
@@ -0,0 +1,14 @@
+package app.web.appsowmin.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "WELCOME TO SPRING BOOT APPLICATION!";
+    }
+
+}
